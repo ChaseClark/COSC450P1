9c6fa1255629f51de3c04cc440f3f6389f8a9ff8
Chase Clark <chase_clark07@hotmail.com>
2021-10-12 13:55:54 -0400

    file IO working

--- matrix.c
+++ matrix.c
@@ -1,17 +1,67 @@
 #include <stdio.h>
 
+// init variables
+int SIZE;
+int matrixA[10000][10000];
+int matrixB[10000][10000];
+int c;
+int inputArray[10000];
+int currentNumber;
+FILE *inputFile;
+FILE *outputFile;
+
+void process_input_file()
+{
+    // read all numbers from input file
+    // open the input file
+    inputFile = fopen("COSC450_P1_Data.txt", "r");
+    if (inputFile == NULL)
+    {
+        // file not found
+        printf("Input File not found!\n");
+        exit(-1);
+    }
+
+    //index to keep track of which char we are on
+    int idx = 0;
+    // put all ints into a temp array
+    // read file number by number
+    while (fscanf(inputFile, "%d", &currentNumber) == 1) // check if there is a next digit
+    {
+        inputArray[idx] = currentNumber;
+        printf("Added %d to the array.\n", currentNumber);
+        idx++;
+    }
+    // close file when done
+    fclose(inputFile);
+    // record idx in a size variable so that we can track the size of our arrays
+    SIZE = idx + 1;
+}
+
+// function that writes array to output file
+void print_to_file()
+{
+    // remove("COSC450_P1_Output.txt");
+    outputFile = fopen("COSC450_P1_Output.txt", "w");
+    if (outputFile == NULL)
+    {
+        // file not found
+        printf("Output file not found!\n");
+        exit(-1);
+    }
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        fprintf(outputFile, "Some text: %d\n", inputArray[i]);
+    }
+
+    fclose(outputFile);
+    return;
+}
+
 // main function of program
 int main()
 {
-    // read all numbers from input file
-    int c;
-    FILE *file;
-    file = fopen("COSC450_P1_Data.txt", "r");
-    if (file)
-    {
-        while ((c = getc(file)) != EOF)
-            putchar(c);
-        fclose(file);
-    }
+    process_input_file();
+    print_to_file();
     return 0;
 }
